41ea7b21d8cdb810ebdb2464545367107e17378e
Mateo mojica <mmojicah.84@gmail.com>
2025-07-31 21:14:49 -0500

    Connect the database to the project

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+import models
+from database import engine
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
 @app.get('/health-check')
 def root_path():
