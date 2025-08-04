37db364c6dfef2f0b2782e73457ecb324e0f5941
Mateo mojica <mmojicah.84@gmail.com>
2025-08-03 21:15:33 -0500

    add endpoints to create and update a todo

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, Path, status
+from pydantic import BaseModel, Field
 from database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
@@ -17,6 +18,12 @@ def get_db():
 
 db_dependency = Annotated[Session,Depends(get_db)]
 
+class TodoRequest(BaseModel):
+  title: str = Field(min_length=3)
+  description: str = Field(min_length=3, max_length=100)
+  priority: int = Field(gt=0, lt=6)
+  complete: bool
+
 @app.get('/health-check')
 def root_path():
   return { "status": "ok" }
@@ -34,3 +41,25 @@ def get_single_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     return todo
     
   raise HTTPException(status_code=404, detail='Todo not found')
+
+@app.post('/todos', status_code=status.HTTP_201_CREATED)
+def create_todo(db: db_dependency, req: TodoRequest):
+  todo = Todos(**req.model_dump())
+
+  db.add(todo)
+  db.commit()
+
+@app.put('/todos/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
+def update_todo(db: db_dependency, req: TodoRequest, todo_id: int=Path(gt=0)):
+  todo = db.query(Todos).filter(Todos.id == todo_id).first()
+
+  if todo is None:
+    raise HTTPException(status_code=404, detail='Todo not found')
+  
+  todo.title = req.title
+  todo.description = req.description
+  todo.priority = req.priority
+  todo.complete = req.complete
+
+  db.add(todo)
+  db.commit()
