93bc0e36496382575431cfa443fcda841ef48233
Mateo mojica <mmojicah.84@gmail.com>
2025-08-04 21:14:17 -0500

    Add functionality to create a user

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ pipenv install "uvicorn[standard]"
 ### Installing dependecies with just pip
 Create the `requirements.tx` from the pipfile
 ```
-pipenv requirements
+pipenv requirements >> requirements.txt
 ```
 
 
@@ -16,6 +16,8 @@ Then run
 ```
 pip install -r requirements.txt
 ```
+
+Do these steps every time a new dependecy is installed through pipenv
 
 ### Running a FastAPi application
 ```
@@ -28,4 +30,4 @@ Or
 fastapi run <file_name_w_ext>
 ```
 
-[Swagger UI](http://localhost:8000/docs)
+[Swagger UI](http://localhost:3929/docs)
--- todo_app/routers/auth.py
+++ todo_app/routers/auth.py
@@ -1,7 +1,47 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends, status
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
-@router.get('/auth')
-def get_user():
-  return { "user": "authenticated" }
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+class CreateUserRequest(BaseModel):
+  username: str
+  email: str
+  first_name: str
+  last_name: str
+  password: str
+  role: str
+
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
+
+db_dependency = Annotated[Session,Depends(get_db)]
+
+
+@router.post('/auth', status_code=status.HTTP_201_CREATED)
+def create_user(db: db_dependency, req: CreateUserRequest):
+  user = Users(
+    username=req.username,
+    email=req.email,
+    first_name=req.first_name,
+    last_name=req.last_name,
+    role=req.role,
+    hashed_password=bcrypt_context.hash(req.password)
+  )
+
+  db.add(user)
+  db.commit()
+
+  return user
