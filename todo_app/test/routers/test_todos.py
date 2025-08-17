a04801c24741bc4b3fbaa56a00c259ff48f8f71b
Mateo mojica <mmojicah.84@gmail.com>
2025-08-16 22:01:41 -0500

    implement utils file in the todos tests

--- todo_app/test/routers/test_todos.py
+++ todo_app/test/routers/test_todos.py
@@ -11,26 +11,8 @@ from sqlalchemy.orm import sessionmaker
 from database import Base
 from main import app
 from models import Todos
-import utils
 from routers.todos import get_current_user, get_db
-
-
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@pgdb:5432/test_todosappdb'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, poolclass=StaticPool)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
-
-def override_get_db():
-  db = TestingSessionLocal()
-  try:
-    yield db
-  finally:
-    db.close()
-
-
-def override_get_current_user():
-  return {"username": "testinguser", "id": 1, "user_role": "admin"}
+from test.utils import TestingSessionLocal, override_get_db, override_get_current_user, engine
 
 
 app.dependency_overrides[get_db] = override_get_db
